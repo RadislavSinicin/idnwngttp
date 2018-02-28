74d5f01e34168c1ea545a20f283be95b6854e524
Radislav Sinicin <radik200058@gmail.com>
2018-02-28 09:53:57 +0200

    FACE

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         cheslo=cheslo+100-100+peslo;
         System.out.println("Ты в программировании полный "+cheslo);
         System.out.println("FACE");
+        System.out.println("Ptaha i Guf");
     }
 }
