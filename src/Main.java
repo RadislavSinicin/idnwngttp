3cffdc06206ca87eb56da7994b8a82d8d83eebdf
Radislav Sinicin <radik200058@gmail.com>
2018-02-28 09:37:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cheslo=0, peslo=0;
+        cheslo=cheslo+100-100+peslo;
+        System.out.println("Ты в программировании полный "+cheslo);
+    }
+}
