68ecaf4208570c705c3ada36979f384d095dc3d0
Radislav Sinicin <radik200058@gmail.com>
2018-02-28 09:42:50 +0200

    Lil Pump

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,5 +7,6 @@ public class Main {
         int cheslo=0, peslo=0;
         cheslo=cheslo+100-100+peslo;
         System.out.println("Ты в программировании полный "+cheslo);
+        System.out.println("LIL PUMP ");
     }
 }
